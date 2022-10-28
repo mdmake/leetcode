3b188e9a900a1d878476916afe921b7dd83c4a14
mdmake <mmake.md@gmail.com>
2022-10-28 08:21:15 +0300

    739. Daily Temperatures

---
+++ problems/739. Daily Temperatures.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        rez = [0]*len(temperatures)
+        stack = []
+
+        for i in range(len(temperatures)-1, -1,-1):
+            while len(stack)>0 and stack[-1][1] <= temperatures[i]:
+                stack.pop()
+            if len(stack)>0:
+                rez[i] = stack[-1][0] - i
+            else:
+                rez[i] = 0
+
+            stack.append((i, temperatures[i]))
+
+        return rez
